5bfad4cd551d5d91adc138be11739ce587b64f01
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2022-02-12 16:07:23 -0500

    setup react-router-dom with routes

--- pal-fe/src/App.js
+++ pal-fe/src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Button } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          PurduePAL Frontend
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>PurduePAL Frontend</p>
+
+        <Button href="/" variant="contained">
           Learn React
-        </a>
+        </Button>
       </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }

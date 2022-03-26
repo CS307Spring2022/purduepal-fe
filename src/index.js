b495c5f36bc8a6bea148128f448297556ed806ac
Chintan Sawla <59383889+sawlachintan@users.noreply.github.com>
2022-03-26 16:18:03 -0400

    test publish 2

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/purduepal-fe">
       <App />
     </BrowserRouter>
   </React.StrictMode>,

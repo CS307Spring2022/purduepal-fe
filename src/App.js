c75a8ab2a613eaa02994eaebf33383630bb0316f
rishyraj <rishyraj10@gmail.com>
2022-03-29 18:21:58 -0400

    unified app.js with changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, createContext } from "react";
+import { useState, useEffect} from "react";
 import "./App.css";
 import {
   Stack,
@@ -6,7 +6,7 @@ import {
   createTheme,
   useMediaQuery,
 } from "@mui/material";
-import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { VerticalNavbar } from "./components/VerticalNavbar";
 import { Navbar } from "./components/Navbar";
@@ -16,6 +16,8 @@ import { SignIn } from "./pages/SignIn/SignIn";
 import { CreatePost } from "./components/CreatePost";
 import { SignUp } from "./pages/SignUp/SignUp";
 import { ForgotPassword } from "./pages/ForgotPassword/ForgotPassword";
+import { PostThread } from "./pages/PostThread/PostThread";
+import { TopicFeed } from "./pages/Explore/TopicFeed";
 
 import { getDesignTokens } from "./themes/Theme";
 import { Saved } from "./pages/Saved/Saved";
@@ -23,6 +25,7 @@ import { Notications } from "./pages/Notifications/Notifications";
 import { Settings } from "./pages/Settings/Settings";
 
 import GlobalState from "./contexts/GlobalStates";
+import { Error404 } from "./components/404";
 
 // export const UserContext = createContext();
 
@@ -41,6 +44,21 @@ function App() {
   // ); //temporary till signin is actually implemented
 
   // console.log(localStorage.email);
+  const [isOnPosts,setIsOnPosts] = useState(
+    location.pathname === "/home" ||
+    location.pathname.includes("postThread")
+  )
+  
+  const [on404, setOn404] = useState(
+    location.pathname === "/404"
+  );
+
+  useEffect(() => {
+    setOn404(location.pathname==="/404");
+    setIsOnPosts(location.pathname === "/home" ||
+    location.pathname.includes("postThread"))
+  },[location.pathname])
+
   const [isSignedIn, setIsSignedIn] = useState(
     localStorage.email !== undefined
   )
@@ -61,19 +79,23 @@ function App() {
             maxWidth: "100vw",
           }}
         >
-          {isSignedIn ? navigation : null}
+          {isSignedIn && !on404 ? navigation : null}
           <Routes>
             <Route path="/" element={<SignIn />} />
             <Route path="/signup" element={<SignUp />} />
             <Route path="/forgotPassword" element={<ForgotPassword />} />
             <Route path="/home" element={<Home />} />
             <Route path="/explore" element={<Explore />} />
-            <Route path="/:profileName" element={<Profile />} />
+            <Route path="/topicFeed" element={<TopicFeed />} />
+            <Route path="/profile" element={<Profile />} />
             <Route path="/saved" element={<Saved />} />
             <Route path="/notifications" element={<Notications />} />
             <Route path="/settings" element={<Settings />} />
+            <Route path="/postThread" element={<PostThread/>} />
+            <Route path="/404" element={<Error404/>} />
+            <Route path="*" element={<Navigate replace to="/404" />} />
           </Routes>
-          {isSignedIn ? <CreatePost /> : null}
+          {isSignedIn && isOnPosts && !on404 ? <CreatePost /> : null}
         </Stack>
       </ThemeProvider>
     </GlobalState.Provider>

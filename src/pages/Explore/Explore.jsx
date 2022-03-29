09d5f0b8f434c5a8e2865265fc12401f439b8300
rishyraj <rishyraj10@gmail.com>
2022-03-29 18:19:20 -0400

    Added Topic Exploration Page

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,11 +1,51 @@
-import { Button, Stack } from "@mui/material";
+import { IconButton, Typography, Stack, Card, CardHeader, useTheme } from "@mui/material";
+import {NotificationsOutlined as FollowEmptyIcon, Explore as ExploreIcon } from "@mui/icons-material";
 import { Navigate } from "react-router-dom";
 import { useContext } from "react";
 
 import GlobalState from "../../contexts/GlobalStates";
 
+const topics = [
+  {name:"Marvel"}, 
+  {name:"Twitter"},
+  {name:"DC"},
+  {name:"Netflix"},
+  {name:"Cricket"}];
+
+export const TopicCard = ({ data }) => {
+  return (
+    <Card
+    sx={{
+      padding: "10px",
+      width: "80vw",
+      maxWidth: { sm: 400, md: 500, lg: 600, xl: 600 },
+      maxHeight: "150vh",
+      borderRadius: "15px",
+      backgroundColor: "#121212",
+    }}
+  >
+    <CardHeader title={data.name} />
+    <IconButton href={"/purduepal-fe/topicFeed?topic="+data.name}>
+      <ExploreIcon color="primary"/>
+      <Typography color="primary" sx={{marginLeft: 1}}>
+        Explore
+      </Typography>
+    </IconButton>
+    <IconButton>
+      <FollowEmptyIcon color="primary"/>
+      <Typography color="primary" sx={{marginLeft: 1}}>
+        Follow
+      </Typography>
+    </IconButton>
+  </Card>
+  )
+
+}
+
+
 export const Explore = () => {
-  const [isSignedIn,setIsSignedIn] = useContext(GlobalState);
+  const theme = useTheme();
+  const [isSignedIn] = useContext(GlobalState);
 
   if (!isSignedIn) {
     return <Navigate to="/"/>;
@@ -13,12 +53,21 @@ export const Explore = () => {
 
   return (
     <Stack
-      width="100vw"
+      width={"100vw"}
       justifyContent={"center"}
       alignItems={"center"}
+      spacing={2}
       minHeight={"100vh"}
+      overflow={"auto"}
+      mt={2}
+      sx={{ marginLeft: { xs: "0px", sm: "75px", md: "200px", lg: "200px" } }}
     >
-      <Button>Explore page</Button>
+      <Typography sx={{fontSize: 50}} color={theme.palette.primary.main}>
+        Explore Topics
+      </Typography>
+      {topics.map((d, i) => {
+        return <TopicCard key={i} data={d} />;
+      })}
     </Stack>
   );
 };
---
+++ src/pages/Explore/TopicFeed.jsx
@@ -0,0 +1,135 @@
+import { useTheme } from "@mui/material/styles";
+import { Stack, Typography } from "@mui/material";
+import { Content } from "../../components/Content/Content";
+import { useState, useContext } from "react";
+import { useEffect } from "react";
+import { Navigate } from "react-router-dom";
+import GlobalState from "../../contexts/GlobalStates";
+
+const sampleData = [
+  {
+    uuid: "1aklsdf8asdfhj",
+    name: "Dr. Stephen Strange",
+    username: "drstrange",
+    date: "10/20/16",
+    topic: "Marvel",
+    content:
+      "It's not Mr. Strange, not Master Strange. It's Doctor Strange! When I became a doctor, I swore to do no harm.",
+    img: null,
+    up: 14000605,
+    down: 0,
+    parentId: ""
+  },
+  {
+    uuid: "0756sd8asdsdfhj",
+    name: "Tony Stark",
+    username: "ironman",
+    date: "05/02/08",
+    topic: "Marvel",
+    content: "I. am. IronMan.",
+    img: null,
+    up: 3000,
+    down: 0,
+    parentId: ""
+  },
+  {
+    uuid: "agg6jgsgdsbsd2gj",
+    name: "Steve Rogers",
+    username: "captainamerica",
+    date: "07/19/11",
+    topic: "Marvel",
+    content: "Avengers... Assemble",
+    img: null,
+    up: 75,
+    down: 0,
+    parentId: ""
+  },
+  {
+    uuid: "6zbkp4s4a43sghgeha",
+    name: "Thor",
+    username: "godofthunder",
+    date: "11/11/11",
+    topic: "Marvel",
+    content: "Stormbreaker",
+    img: "https://i0.wp.com/thenewsfetcher.com/wp-content/uploads/2020/01/45dc07a7fec3414781000b10577e539e.jpeg",
+    up: 1000,
+    down: 0,
+    parentId: ""
+  },
+  {
+    uuid: "b04uwqual3s2f0j",
+    name: "Hulk",
+    username: "greenguy",
+    date: "07/19/11",
+    topic: "Marvel",
+    content: "Strongest Avenger",
+    img: null,
+    up: 80,
+    down: 0,
+    parentId: ""
+  }
+];
+
+const RecordsList = () => {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    async function getRecords() {
+      const response = await fetch("http://127.0.0.1:5000/posts/");
+
+      if (!response.OK) {
+        const messsage = `An error occurred: ${response.statusText}`;
+        console.log(messsage);
+        return;
+      }
+
+      const records = await response.json();
+      setRecords(records);
+    }
+
+    getRecords();
+    return;
+  }, [records.length]);
+
+  // console.log(records);
+  return <h1>Something</h1>;
+};
+
+export const TopicFeed = () => {
+  const theme = useTheme();
+  const [isSignedIn] = useContext(GlobalState);
+
+  if (!isSignedIn) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <Stack
+      width={"100vw"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      spacing={2}
+      minHeight={"100vh"}
+      overflow={"auto"}
+      mt={2}
+      sx={{ marginLeft: { xs: "0px", sm: "75px", md: "200px", lg: "200px" } }}
+    >
+      {sampleData.map((data, index) => {
+        return <Content key={index} data={data} />;
+      })}
+      <Typography variant="p" sx={{ fontSize: "30px" }} color="primary">
+        End of Posts!
+      </Typography>
+      {<RecordsList />}
+      <div>
+        <svg width={100} height={50}>
+          <rect
+            width="100%"
+            height={50}
+            fill={theme.palette.background.default}
+          ></rect>
+        </svg>
+      </div>
+    </Stack>
+  );
+};
